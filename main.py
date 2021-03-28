8c23dba01ad9b848c9e85cdcdff97f82aded5d1d
Thomas Holland <59330596+thomas-holland@users.noreply.github.com>
2021-03-28 14:45:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ async def on_ready(): # start up call
     print('We have logged in as {0.user}'.format(client))
 
 @bot.command()
-async def test(ctx, *args):
-    await ctx.send(args)
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 
 BOT_KEY = os.environ["BOT_KEY"] # gets the bot key from the host platform (so it is not hosted on github)
